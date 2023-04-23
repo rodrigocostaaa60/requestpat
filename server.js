af08b29accb2e527462375a77688d8e04563346e
Glitch (cristaltelecom) <none>
2023-04-23 05:24:52 +0000

    🎓🍗 Checkpoint ./server.js:57368257/12715

--- server.js
+++ server.js
@@ -15,51 +15,60 @@ app.post("/wbhcristal", function (request, response) {
   var intentName = request.body.queryResult.intent.displayName;
 
   if (intentName == "patrimonios") {
-    var pat1 = request.body.queryResult.parameters["pat1"];
+    var searchTerm = Object.keys(request.body.queryResult.parameters)[0];
+    var searchValue = request.body.queryResult.parameters[searchTerm];
 
     axios
       .get("https://sheetdb.io/api/v1/w0t5ql0i871kj")
       .then((res) => {
-        res.data.map((person) => {
-          if (person.pat1 === pat1)
-            response.json({
-              fulfillmentText:
-                "Detalhes para o Patrimônio: " +
-                pat1 +
-                ":\n" +
-                "ID da Ordem de Serviço: " +
-                person.id +
-                "\n" +
-                "Nome do Cliente: " +
-                person.nome +
-                "\n" +
-                "Patrimônio Nº1: " +
-                person.pat1 +
-                "\n" +
-                "Patrimônio Nº2: " +
-                person.pat2 +
-                "\n" +
-                "Data da Configuração: " +
-                person.data +
-                "\n" +
-                "Hora da Configuração: " +
-                person.hora +
-                "\n" +
-                "Tipo da Configuração: " +
-                person.tipo +
-                "\n" +
-                "Responsável da Configuração: " +
-                person.responsavel +
-                "\n" +
-                "Link do Resultado: " +
-                person.speedtest +
-                "\n",
-            });
+        var results = res.data.filter((person) => {
+          return person[searchTerm] === searchValue;
         });
+        if (results.length > 0) {
+          var responseText = "Detalhes para " + searchTerm + " = " + searchValue + ":\n";
+          for (var i = 0; i < results.length; i++) {
+            var person = results[i];
+            responseText +=
+              "ID da Ordem de Serviço: " +
+              person.id +
+              "\n" +
+              "Nome do Cliente: " +
+              person.nome +
+              "\n" +
+              "Patrimônio Nº1: " +
+              person.pat1 +
+              "\n" +
+              "Patrimônio Nº2: " +
+              person.pat2 +
+              "\n" +
+              "Data da Configuração: " +
+              person.data +
+              "\n" +
+              "Hora da Configuração: " +
+              person.hora +
+              "\n" +
+              "Tipo da Configuração: " +
+              person.tipo +
+              "\n" +
+              "Responsável da Configuração: " +
+              person.responsavel +
+              "\n" +
+              "Link do Resultado: " +
+              person.speedtest +
+              "\n\n";
+          }
+          response.json({
+            fulfillmentText: responseText,
+          });
+        } else {
+          response.json({
+            fulfillmentText: "Não encontrei nenhuma configuração com " + searchTerm + " = " + searchValue,
+          });
+        }
       })
       .catch((err) => {
         console.error(err);
-        response.status(500).send("Erro interno do servidor");
+        response.status(500).send("Oops, deu erro no servidor... :/");
       });
   } else {
     response.status(400).send("Poxa vida, não achei o que você pediu! Tenta de novo, vai que agora dá certo!");
