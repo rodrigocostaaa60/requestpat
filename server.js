7745a9b3589c99b6e1dfd0a15338b69f1a5cf31b
Glitch (cristaltelecom) <none>
2023-04-23 06:36:51 +0000

    🚥🏢 Checkpoint ./server.js:57368257/6351

--- server.js
+++ server.js
@@ -1,82 +1,22 @@
-const WebhookClient = require("dialogflow-fulfillment");
-const fastify = require("fastify")();
-const axios = require('axios');
+const WebhookClient = require('dialogflow-fulfillment');
+const bodyParser = require("body-parser");
+const axios = require ("axios");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/wbhcristal', function(request,response){
+ const agent = new WebhookClient({ request:request, response:response });
 
 
-fastify.register(require("fastify-formbody"));
 
-fastify.post("/wbhcristal", function (request, response) {
-  var intentName = request.body.queryResult.intent.displayName;
 
-  if (intentName == "patrimonios") {
-    var searchTerm = request.body.queryResult.parameters["searchTerm"];
+ if(intentName == 'patrimonios'){
+ var Pedido = request.body.queryResult.parameters['Pedido'];
 
-    axios
-      .get("https://sheetdb.io/api/v1/w0t5ql0i871kj")
-      .then((res) => {
-        const results = res.data.filter((person) => {
-          return Object.keys(person).some(
-            (key) =>
-              person[key].toLowerCase().includes("pat1") &&
-              person[key].toLowerCase().includes("pat2")
-          );
-        });
-
-        let fulfillmentText = "";
-        results.forEach((person) => {
-          fulfillmentText +=
-            "Detalhes para o Patrimônio: " +
-            person.pat1 +
-            ":\n" +
-            "ID da Ordem de Serviço: " +
-            person.id +
-            "\n" +
-            "Nome do Cliente: " +
-            person.nome +
-            "\n" +
-            "Patrimônio Nº1: " +
-            person.pat1 +
-            "\n" +
-            "Patrimônio Nº2: " +
-            person.pat2 +
-            "\n" +
-            "Data da Configuração: " +
-            person.data +
-            "\n" +
-            "Hora da Configuração: " +
-            person.hora +
-            "\n" +
-            "Tipo da Configuração: " +
-            person.tipo +
-            "\n" +
-            "Responsável da Configuração: " +
-            person.responsavel +
-            "\n" +
-            "Link do Resultado: " +
-            person.speedtest +
-            "\n\n";
-        });
-
-        response.send({
-          fulfillmentText: fulfillmentText,
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        response.status(500).send("Oops, deu erro no servidor... :/");
-      });
-  } else {
-    response
-      .status(400)
-      .send(
-        "Poxa vida, não achei o que você pediu! Tenta de novo, vai que agora dá certo!"
-      );
-  }
-});
-
-fastify.listen(3000, (err) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-});
+ return axios.get("LINK").then(res => {
+ res.data.map(person => {
+ if (person.Pedido === Pedido)
+ response.json({"fulfillmentText" :"Detalhes para o pedido "+Pedido+":"+"\n"+
+ "Nome: "+person.Nome+"\n"+
+ "Status: "+person.Status
+ });
