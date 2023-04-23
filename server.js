0a7cd517db29e2f798cf03163052c5308a644260
Glitch (cristaltelecom) <none>
2023-04-23 06:48:54 +0000

    ⚾️⛱ Checkpoint ./server.js:57368257/2132

--- server.js
+++ server.js
@@ -11,12 +11,23 @@ app.post('/wbhcristal', function(request,response){
 
 
  if(intentName == 'patrimonios'){
- var Pedido = request.body.queryResult.parameters['Pedido'];
+  var pat1 = request.body.queryResult.parameters['pat1'];
+  var pat2 = request.body.queryResult.parameters['pat2'];
 
- return axios.get("LINK").then(res => {
+
+ return axios.get("https://sheetdb.io/api/v1/w0t5ql0i871kj").then(res => {
  res.data.map(person => {
- if (person.Pedido === Pedido)
- response.json({"fulfillmentText" :"Detalhes para o pedido "+Pedido+":"+"\n"+
- "Nome: "+person.Nome+"\n"+
- "Status: "+person.Status
+ if (person.pat1 === pat1)
+ response.json({"fulfillmentText" :"Detalhes para o Patrimônio: "+Pedido+":"+"\n"+
+ "ID da Ordem de Serviço: "+person.id+"\n"+
+ "Nome do Cliente: "+person.Nome+"\n"+
+"Patrimônio Nº1: "+person.pat1+"\n"+
+"Patrimônio Nº2: "+person.pat2+"\n"+
+"Data da Configuração: "+person.data+"\n"+
+"Hora da Configuração: "+person.hora+"\n"+
+"Tipo da Configuração: "+person.tipo+"\n"+
+"Responsável da Configuração: "+person.responsavel+"\n"+
+"Link do Resultado: "+person.speedtest+"\n"+
+
+
  });
