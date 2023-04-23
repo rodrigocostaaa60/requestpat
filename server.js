4505aca15d64698e009e4dd34f7ec1412cec0199
Glitch (glitch-hello-node) <none>
2023-04-23 03:50:53 +0000

    🎆🍙 Checkpoint ./package.json:57368257/120 ./server.js:57368257/1387

--- server.js
+++ server.js
@@ -1,34 +1,22 @@
-const express = require("express"); 
-const app = express(); 
-
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-app.use(express.static("public")); 
-app.get("/", function(request, response) {
-response.sendFile(__dirname + "/views/index.html");
+app.use(express.static("public"));
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/wbhcristal", function(request, response) {
-var intentName = request.body.queryResult.intent.displayName;
+app.post("/wbhcristal", function (request, response) {
+  var intentName = request.body.queryResult.intent.displayName;
 
-  
-  
-
-  
-if (intentName == "cristal2020*") {
-
- response.json({ "fulfillmentText" : "Isso aqui é um Teste"});
- }
+  if (intentName == "cristal2020*") {
+    response.json({ fulfillmentText: "Isso aqui é um Teste" });
+  }
 });
 
-
-
-
-
-const listener = app.listen(process.env.PORT, function() {
-console.log("Your app is listening on port " + listener.address().port);
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
