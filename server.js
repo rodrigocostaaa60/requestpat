16f393d11314b851d5c72b0f65c77a1888b19ed2
Glitch (cristaltelecom) <none>
2023-04-23 08:12:09 +0000

    🏥🌅 Checkpoint ./server.js:57368257/3999

--- server.js
+++ server.js
@@ -11,45 +11,24 @@ app.post("/wbhcristal", async function (request, response) {
   const agent = new WebhookClient({ request: request, response: response });
   const intentName = agent.intent;
 
-  if (intentName === "teste") {
-    const pat1 = request.body.queryResult.parameters["pat1"];
+  if(intentName == 'teste') {
+    var pat1 = request.body.queryResult.parameters['pat1'];
 
     try {
-      const res = await axios.get("https://sheetdb.io/api/v1/w0t5ql0i871kj");
-      res.data.map((person) => {
+      const res = await axios.get("https://cristaltelecom.glitch.me/wbhcristal");
+      res.data.map(person => {
         if (person.pat1 === pat1) {
           response.json({
-            fulfillmentText:
-              "Detalhes para o Patrimônio:" +
-              "\n" +
-              "\n" +
-              "ID da Ordem de Serviço: " +
-              person.id +
-              "\n" +
-              "Nome do Cliente: " +
-              person.Nome +
-              "\n" +
-              "Patrimônio Nº1: " +
-              person.pat1 +
-              "\n" +
-              "Patrimônio Nº2: " +
-              person.pat2 +
-              "\n" +
-              "Data da Configuração: " +
-              person.data +
-              "\n" +
-              "Hora da Configuração: " +
-              person.hora +
-              "\n" +
-              "Tipo da Configuração: " +
-              person.tipo +
-              "\n" +
-              "Responsável da Configuração: " +
-              person.responsavel +
-              "\n" +
-              "Link do Resultado: " +
-              person.speedtest +
-              "\n",
+            "fulfillmentText": "Detalhes para o pat1 " + pat1 + ":" + "\n" +
+              "ID da Ordem de Serviço: " + person.id + "\n" +
+              "Nome do Cliente: " + person.Nome + "\n" +
+              "Patrimônio Nº1: " + person.pat1 + "\n" +
+              "Patrimônio Nº2: " + person.pat2 + "\n" +
+              "Data da Configuração: " + person.data + "\n" +
+              "Hora da Configuração: " + person.hora + "\n" +
+              "Tipo da Configuração: " + person.tipo + "\n" +
+              "Responsável da Configuração: " + person.responsavel + "\n" +
+              "Link do Resultado: " + person.speedtest + "\n"
           });
         }
       });
