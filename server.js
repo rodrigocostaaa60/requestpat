4793049eade4f1eb4918a192fbf342b958bf3645
Glitch (cristaltelecom) <none>
2023-04-23 07:00:54 +0000

    👜🛀 Checkpoint ./server.js:57368257/15349

--- server.js
+++ server.js
@@ -1,33 +1,64 @@
-const WebhookClient = require('dialogflow-fulfillment');
+const WebhookClient = require("dialogflow-fulfillment");
 const bodyParser = require("body-parser");
-const axios = require ("axios");
+const axios = require("axios");
+const express = require("express");
+const app = express();
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/wbhcristal', function(request,response){
- const agent = new WebhookClient({ request:request, response:response });
+app.post("/wbhcristal", async function (request, response) {
+  const agent = new WebhookClient({ request: request, response: response });
 
+  if (intentName == "patrimonios") {
+    var pat1 = request.body.queryResult.parameters["pat1"];
+    var pat2 = request.body.queryResult.parameters["pat2"];
 
+    try {
+      const res = await axios.get("https://sheetdb.io/api/v1/w0t5ql0i871kj");
+      res.data.map((person) => {
+        if (person.pat1 === pat1) {
+          response.json({
+            fulfillmentText:
+              "Detalhes para o Patrimônio:" +
+              "\n" +
+              "\n" +
+              "ID da Ordem de Serviço: " +
+              person.id +
+              "\n" +
+              "Nome do Cliente: " +
+              person.Nome +
+              "\n" +
+              "Patrimônio Nº1: " +
+              person.pat1 +
+              "\n" +
+              "Patrimônio Nº2: " +
+              person.pat2 +
+              "\n" +
+              "Data da Configuração: " +
+              person.data +
+              "\n" +
+              "Hora da Configuração: " +
+              person.hora +
+              "\n" +
+              "Tipo da Configuração: " +
+              person.tipo +
+              "\n" +
+              "Responsável da Configuração: " +
+              person.responsavel +
+              "\n" +
+              "Link do Resultado: " +
+              person.speedtest +
+              "\n",
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
 
-
- if(intentName == 'patrimonios'){
-  var pat1 = request.body.queryResult.parameters['pat1'];
-  var pat2 = request.body.queryResult.parameters['pat2'];
-
-
- return axios.get("https://sheetdb.io/api/v1/w0t5ql0i871kj").then(res => {
- res.data.map(person => {
- if (person.pat1 === pat1)
- response.json({"fulfillmentText" :"Detalhes para o Patrimônio: "+Pedido+":"+"\n"+
- "ID da Ordem de Serviço: "+person.id+"\n"+
- "Nome do Cliente: "+person.Nome+"\n"+
-"Patrimônio Nº1: "+person.pat1+"\n"+
-"Patrimônio Nº2: "+person.pat2+"\n"+
-"Data da Configuração: "+person.data+"\n"+
-"Hora da Configuração: "+person.hora+"\n"+
-"Tipo da Configuração: "+person.tipo+"\n"+
-"Responsável da Configuração: "+person.responsavel+"\n"+
-"Link do Resultado: "+person.speedtest+"\n"+
-
-
- });
+app.listen(3000, () => {
+  console.log("Servidor iniciado na porta 3000");
+});
