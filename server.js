20d2e0144999d6a4203d53f8b8cca8c1d9d2d426
Glitch (glitch-hello-node) <none>
2023-04-23 03:26:50 +0000

    🐧🐴 Checkpoint ./server.js:57368257/1882

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 const express = require("express"); 
 const app = express(); 
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.use(express.static("public")); 
+app.get("/", function(request, response) {
+response.sendFile(__dirname + "/views/index.html");
+});
+
+app.post(‘/wbhcristal’, function(request, response) {
+var intentName = request.body.queryResult.intent.displayName;
+
+
+
+
+const listener = app.listen(process.env.PORT, function() {
+console.log("Your app is listening on port " + listener.address().port);
+});
