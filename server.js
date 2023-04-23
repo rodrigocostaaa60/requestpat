f4cab7951d706a27b331c9db1c3f3ab56fc40cca
Glitch (cristaltelecom) <none>
2023-04-23 05:12:55 +0000

    👞🍴 Checkpoint ./server.js:57368257/7228

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const axios = require ("axios");
+const axios = require("axios");
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
@@ -13,28 +14,58 @@ app.get("/", function (request, response) {
 app.post("/wbhcristal", function (request, response) {
   var intentName = request.body.queryResult.intent.displayName;
 
- if(intentName == 'patrimonios'){
- var pat1 = request.body.queryResult.parameters['pat1'];
+  if (intentName == "patrimonios") {
+    var pat1 = request.body.queryResult.parameters["pat1"];
 
- return axios.get("https://sheetdb.io/api/v1/w0t5ql0i871kj").then(res => {
- res.data.map(person => {
- if (person.pat1 === pat1)
- response.json({"fulfillmentText" :"Detalhes para o Patrimônio: "+pat1+":"+"\n"+
- "ID da Ordem de Serviço: "+person.id+"\n"+
- "Nome do Cliente: "+person.nome+"\n"+
- "Patrimônio Nº1: "+person.pat1+"\n"+
- "Patrimônio Nº2: "+person.pat2+"\n"+
-"Data da Configuração: "+data+"\n"+
-"Hora da Configuração: "+hora+"\n"+
-"Tipo da Configuração: "+tipo+"\n"+
-"Responsável da Configuração: "+responsavel+"\n"+
-               
-               });
- });
- });
- }
+    axios
+      .get("https://sheetdb.io/api/v1/w0t5ql0i871kj")
+      .then((res) => {
+        res.data.map((person) => {
+          if (person.pat1 === pat1)
+            response.json({
+              fulfillmentText:
+                "Detalhes para o Patrimônio: " +
+                pat1 +
+                ":\n" +
+                "ID da Ordem de Serviço: " +
+                person.id +
+                "\n" +
+                "Nome do Cliente: " +
+                person.nome +
+                "\n" +
+                "Patrimônio Nº1: " +
+                person.pat1 +
+                "\n" +
+                "Patrimônio Nº2: " +
+                person.pat2 +
+                "\n" +
+                "Data da Configuração: " +
+                person.data +
+                "\n" +
+                "Hora da Configuração: " +
+                person.hora +
+                "\n" +
+                "Tipo da Configuração: " +
+                person.tipo +
+                "\n" +
+                "Responsável da Configuração: " +
+                person.responsavel +
+                "\n" +
+                "Link do Resultado: " +
+                person.speedtest +
+                "\n",
+            });
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        response.status(500).send("Erro interno do servidor");
+      });
+  } else {
+    response.status(400).send("Poxa vida, não achei o que você pediu! Tenta de novo, vai que agora dá certo!");
+  }
+});
 
-
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Seu app está ouvindo na porta " + listener.address().port);
 });
